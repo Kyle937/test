ffc9be47e079ce963e155b30700dc5a205f953b4
kyle <kyle@webdev15.local>
2026-01-12 12:31:29 -0500

    ver 1

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # imports
 import random
+import time
+import os
+import sys
 # variables
 loop = True
 correct = ""
@@ -9,59 +12,86 @@ wrongAnswers = 0
 part1 = 0
 part2 = 0
 part3 = 0
+result = 0
 questions = 0
+no = ["n", "no", "f", "wrong"]
+yes = ["y", "yes", "t", "correct", part3]
 # functions
 def question(answer, said):
+    if said in yes:
+        said = "true"
+    elif said in no:
+        said = "false"
     if said == answer:
         return True
     else:
         return False
+def clear_terminal():
+    if sys.platform.startswith('win32'):
+        os.system('cls')
+    else:
+        os.system('clear')
 # code
+clear_terminal()
 print("these questions are all true/false")
 print("how many questions would you like?")
 questions = input()
 if not questions.isnumeric():
     print("nice try, have 5 questions")
-    questions =5
-while questions != 0:
-    questions -= 1
-    randQuestion = random.randint(1,4)
+    questions = 5
+questions = int(questions)
+if questions != 0:
+    while questions != 0:
+        time.sleep(0.1)
+        clear_terminal()
+        questions -= 1
+        randQuestion = random.randint(1,4)
 
-    if randQuestion == 1:
-        print("what will the result of this be")
-        part1 = random.randint(random.randint(1,5),random.randint(10,50))
-        part2 = random.randint(random.randint(1,5),random.randint(10,50))
-        part3 = part1 + part2
-        print(part1,"+",part2,"=",part3)
-        correct = "True"
+        # true add
+        if randQuestion == 1:
+            print("what will the result of this be")
+            part1 = random.randint(random.randint(1,5),random.randint(10,50))
+            part2 = random.randint(random.randint(1,5),random.randint(10,50))
+            part3 = part1 + part2
+            print(part1,"+",part2,"=",part3)
+            correct = "True"
 
-    elif randQuestion == 2:
-        print("what will the result of this be")
-        part1 = random.randint(random.randint(1,5),random.randint(10,50))
-        part2 = random.randint(random.randint(1,5),random.randint(10,50))
-        while part3==part1+part2:
-            part3 = random.randint(random.randint(2,10),random.randint(20,100))
-        print(part1,"+",part2,"=",part3)
-        correct = "False"
+        # false add
+        elif randQuestion == 2:
+            print("what will the result of this be")
+            part1 = random.randint(random.randint(1,5),random.randint(10,50))
+            part2 = random.randint(random.randint(1,5),random.randint(10,50))
+            while part3==part1+part2:
+                part3 = random.randint(random.randint(2,10),random.randint(20,100))
+            print(part1,"+",part2,"=",part3)
+            correct = "False"
 
-    elif randQuestion == 3:
-        print("what will the result of this be")
-        part1 = random.randint(random.randint(1,5),random.randint(10,50))
-        part2 = random.randint(random.randint(1,5),random.randint(10,50))
-        while part3==part1-part2:
-            part3 = random.randint(random.randint(1,5),random.randint(5,15))
-        print(part1,"-",part2,"=",part3)
-        correct = "False"
+        # false subtract
+        elif randQuestion == 3:
+            print("what will the result of this be")
+            part1 = random.randint(random.randint(1,5),random.randint(10,50))
+            part2 = random.randint(random.randint(1,5),random.randint(10,50))
+            while part3==part1-part2:
+                part3 = random.randint(0,random.randint(5,15))
+            print(part1,"-",part2,"=",part3)
+            correct = "False"
 
-    elif randQuestion == 4:
-        print("what will the result of this be")
-        part1 = random.randint(random.randint(1,5),random.randint(10,50))
-        part2 = random.randint(random.randint(1,5),random.randint(10,50))
-        part3 = part1-part2
-        print(part1,"-",part2,"=",part3)
-        correct = "True"
+        # true subtract
+        elif randQuestion == 4:
+            print("what will the result of this be")
+            part1 = random.randint(random.randint(1,5),random.randint(10,50))
+            part2 = random.randint(random.randint(1,5),random.randint(10,50))
+            part3 = part1-part2
+            print(part1,"-",part2,"=",part3)
+            correct = "True"
     
-    if question(correct.lower().strip(), input().lower().strip()):
-        correctAnswers += 1
-    else:
-        wrongAnswers += 1
+        if question(correct.lower().strip(), input().lower().strip()):
+            correctAnswers += 1
+        else:
+            wrongAnswers += 1
+    result = correctAnswers + wrongAnswers
+    clear_terminal()
+    print("you got", str(correctAnswers) +"/"+str(result), "correct")
+else:
+    clear_terminal()
+    print("oh ok")
